d8715cfc9ac7360f283ee53b1172af85e8a307f4
Yael <lustigyael10@gmail.com>
2021-01-13 14:44:50 +0200

    fix: configuation .env names

--- src/config.ts
+++ src/config.ts
@@ -1,6 +1,6 @@
 export const uiConfiguation = {
   uri: {
-    api_gateway_uri: process.env.APR_GETWAY_URI || 'http://localhost:3000',
+    api_gateway_uri: process.env.APR_GETWAY_URI || 'http://localhost:3002',
     auth_service_uri: process.env.AUTH_SERVICE_URI || 'http://localhost:3001',
   },
   roles: {
@@ -9,11 +9,11 @@ export const uiConfiguation = {
     manager_role_value: process.env.MANAGER_ROLE_VALUE || 2,
   },
   length_limitations: {
-    min_length_group_name: process.env.MIN_GROUP_NAME_LENGTH || 2,
-    min_length_tag: process.env.MIN_TAG_LENGTH || 2,
-    min_length_user_search_value: process.env.MIN_USER_SEARCH_VALUE || 2,
+    min_length_group_name: process.env.MIN_LENGTH_GROUP_NAME || 2,
+    min_length_tag: process.env.MIN_LENGTH_TAG_LENGTH || 2,
+    min_length_user_search_value: process.env.MIN_LENGTH_USER_SEARCH_VALUE || 2,
   },
-  token_name: process.env.AUTH_SERVICE_TOKEN || 'friendsToken',
+  token_name: process.env.TOKEN_NAME || 'friendsToken',
 };
 
 export const server = {
